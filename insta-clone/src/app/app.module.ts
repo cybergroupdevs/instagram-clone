bd18525202167f4f0a78b497784b8c72873a2844
balani98 <balanideepanshu92@gmail.com>
2020-02-14 11:16:42 +0530

    image gallery made

--- insta-clone/src/app/app.module.ts
+++ insta-clone/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {  ImageGalleryComponent } from './image-gallery/image-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    ImageGalleryComponent,
+    
   ],
   imports: [
     BrowserModule,
