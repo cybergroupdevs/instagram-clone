d0f243b2168ad3d571b94bc65bc624c54dce4bdc
abharana24 <abha801@gmail.com>
2020-02-16 00:12:58 +0530

    schemas updated

--- insta-node-backend/schemas/user-uploads.js
+++ insta-node-backend/schemas/user-uploads.js
@@ -1,5 +1,5 @@
+const mongoose = require('mongoose');
 const user = require('./user-details');
-var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 module.exports = {
     ownerId: {
