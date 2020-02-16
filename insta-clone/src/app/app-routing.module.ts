2d2eebf2ac57cdb0dfffff61d1173b160ca3ad5d
balani98 <balanideepanshu92@gmail.com>
2020-02-16 22:36:42 +0530

    feed is made responsive

---
+++ insta-clone/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { FeedComponent } from './feed/feed.component';
+import { AppComponent } from './app.component';
+import { ProfileDashboardComponent } from './profile-dashboard/profile-dashboard.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent }      from './login/login.component';
+import { SignupComponent }  from './signup/signup.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/feed', pathMatch: 'full'},
+  { path: 'feed', component: FeedComponent},
+  { path: 'login', component: LoginComponent },
+
+  { path: 'signup', component: SignupComponent },
+
+  {path: 'profile', component: ProfileDashboardComponent}
+  ];
+
+  { path: 'profile', component: ProfileDashboardComponent}
+];
+
+
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+
+export class AppRoutingModule{ }
+
+
+
