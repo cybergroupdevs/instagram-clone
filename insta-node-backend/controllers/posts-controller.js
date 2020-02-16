b19aaa66ec09c445c8b382976593e192004325c2
nangchetnamaw <nangchetnamaw@gmail.com>
2020-02-16 19:47:09 +0530

    follow/unfollow api and ui modification

--- insta-node-backend/controllers/posts-controller.js
+++ insta-node-backend/controllers/posts-controller.js
@@ -17,7 +17,41 @@ class employee{
         }
         
     }
+    async updateFollow(req,res){
+        let followerObj={
+            $inc: {'following':1}
+        }
+        let followedObj={
+            $inc:{'follower':1}
+        }
+        const follower = await model.userModel.follow({"instaHandle": req.body.followerId},followerObj)
+        const folowed = await model.userModel.follow({"instaHandle":req.body.followedId},followerObj)
+        let followerObj={
+            "ownerId" : req.body.followedObj,
+            "followerId": req.body.followerId
+        }
+        let followedObj = {
+            "ownerId": req.body.followerId,
+            "followingId" : req.body.followedId
+        }
+        const follower = await model.followerModel.follow(followerObj);
+        const followed = await model.followingModel.follow(followedObj);
 
+        res.send("now following user")
+    }
+    async updateUnfollow(req,res){
+        let unfollowerObj={
+            $inc: {'following':-1}
+        }
+        let unfollowedObj={
+            $inc:{'follower':-1}
+        }
+        const unfollower = await model.userModel.unfollow({"instaHandle": req.body.unfollowerId},unfollowerObj);
+        const unfollowed = await model.userModel.unfollow({"instaHandle": req.body.unfollowedId},unfollowedObj);
+        const unfollower = await model.followerModel.unfollow({"ownerId": req.body.unfollowedId,"followerId":req.body.unfollowerId});
+        const unfollowed = await model.followingModel.unfollow({"ownerId":req.body.unfollowerId,"followingId":req.body.unfollowedId});
+        res.send("now unfollowed user");
+    }
     async show(req, res){
     }
 
