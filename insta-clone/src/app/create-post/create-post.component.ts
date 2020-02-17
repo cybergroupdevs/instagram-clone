4c7cbe54a9dc411c5feb082c8f14714cd180923f
Shubham Sharma <ss320163@gmail.com>
2020-02-18 03:50:19 +0530

    Storing the image in File System through nodejs

---
+++ insta-clone/src/app/create-post/create-post.component.ts
@@ -0,0 +1,33 @@
+import { FileUploader } from 'ng2-file-upload';
+import { FileSelectDirective } from 'ng2-file-upload';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { ObjectUnsubscribedError } from 'rxjs';
+
+const URL = 'http://localhost:8080/upload';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.css']
+})
+
+export class CreatePostComponent implements OnInit {
+  
+  constructor() { }
+
+  // objectId = "dkcdklcdslknk12knnckn11212n2nen1l";
+
+  public uploader: FileUploader = new FileUploader({
+    url: URL,
+    itemAlias: 'image'
+  });
+
+  ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+    this.uploader.onCompleteItem = (item: any, status: any) => {
+      console.log("Uploaded File details", item, status);
+    };
+  }
+}
